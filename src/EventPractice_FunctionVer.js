7e3fd346e2104fd5f087363e61479371a429e5da
SeyoungKo <seyoungg1223@gmail.com>
2022-07-19 01:29:37 +0900

    Add : functional component
    함수형 컴포넌트 사용 기본

--- src/EventPractice_FunctionVer.js
+++ src/EventPractice_FunctionVer.js
@@ -5,6 +5,12 @@ const EventPractice_FunctionVer = () => { // 함수형 컴포넌트
     const [username, setUsername] = useState(''); // username value를 업데이트
     const [message, setMessage] = useState(''); // message value를 업데이트
     // 이벤트 처리
+    // 아래 주석 핸들러와 동일한 역할을 한다 (인풋이 적을 때는 유용하지만 개수가 많아지면 e.target.name을 활용하는게 더 좋다)
+    // handleChange(e){ 
+    //     this.setState({
+    //         [e.target.name]: e.target.value // 여러개의 state 중 해당하는 state를 동적으로 처리
+    //     });
+    // }
     const onChangeUsername = e => setUsername(e.target.value);
     const onChangeMessage = e => setMessage(e.target.value);
     const onClick = () => {
